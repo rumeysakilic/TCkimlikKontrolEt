23e9484167ff90c3f99113d1be732c32c342750c
Rumeysa KILIÇ <111907567+rumeysakilic@users.noreply.github.com>
2023-07-14 12:16:08 +0300

    TC kimlik kontrol
    Personel sınıfı oluşturulup TC kimlik no'sunun property ile kapsüllenmesi ve ulaşılmak istendiğinde uygun TC no'su mu kontrolünü gerçekleştiren program.

---
+++ Personel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapsullemeveKontrol
+{
+    public class Personel
+    {
+        private string tcKimlikNo;
+        public string TCKIMLIKNO
+        {
+            get
+            {
+                return tcKimlikNo.Substring(0,5)+"******";
+            }
+            set
+            {
+                bool kontrolEt = false;
+                if (value.Length== 11)
+                {
+                    for(int i = 0; i < value.Length; i++)
+                    {
+                        bool sayiMi = Char.IsNumber(value[i]);
+                        if (sayiMi)
+                        {
+                            tcKimlikNo = value;
+                        }
+                        else
+                        {
+                            kontrolEt = true;
+                            break;
+                        }
+                    }
+                    if(kontrolEt)
+                    {
+                        Console.WriteLine("TC kimlik numaranızda geçersiz karakter bulundu!!!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("TC kimlik numaranız 11 karakter olmalı!!!");  
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapsullemeveKontrol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personel P1 = new Personel();
+            P1.TCKIMLIKNO = "12345678989";
+            Console.WriteLine("TC kimlik numaranız:" +P1.TCKIMLIKNO);
+        }
+    }
+}
